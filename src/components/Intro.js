15efac77ee0fe325f7a058a144c93d884fc31897
Daffdhot29 <daff.yunus05@gmail.com>
2025-08-05 19:51:57 +0700

    update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,16 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 const Intro = () => {
     return (
         <div className='introSection'>
-          <Container className='text-white d-flex justify-content-center align-items-center'>
+          <Container className='text-white d-flex text-center justify-content-center align-items-center'>
             <Row>
               <Col> 
                 <div className='title'>Free Movie</div>
                 <div className='title'>Watch your favorite movies for free</div>
+                <div className='introButton mt-4 text-center'>
+                     <Button variant="primary">All List Movie </Button>
+                </div>
               </Col>
             </Row>
           </Container>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,10 +2,10 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavigationBar = () => {
     return (
-        <Navbar  className="mb-4 shadow">
+        <Navbar  className="mb-4 shadow navTrends">
             <Container>
                 <Navbar.Brand href="#home"> Tontonan-Ku </Navbar.Brand>
-                <Nav className="justify-content-end navTrends">
+                <Nav className="justify-content-end ">
                     <Nav.Link href="#trending">Trending</Nav.Link>
                     <Nav.Link href="#action"> Action </Nav.Link>
                 </Nav>
