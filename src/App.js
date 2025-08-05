63422916261e99e95e5657ef753d1590f8393dcf
Daffdhot29 <daff.yunus05@gmail.com>
2025-08-05 14:57:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
 import './style/landingPage.css';
+import { Col, Container, Row } from 'react-bootstrap';
 function App() {
   return (
    <div>
+    {/* Intro Section */}
       <div className='myBG'>
         <NavigationBar/>
       </div> 
+      
    </div>
   );
 }
