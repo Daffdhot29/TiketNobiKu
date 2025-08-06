66b9e3395e2117e2de9bf26e5e6f018da061b3db
Daffdhot29 <daff.yunus05@gmail.com>
2025-08-06 14:40:10 +0700

    update

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,19 +1,26 @@
-import {Card} from 'react-bootstrap';
+import {Card, Col, Container, Row} from 'react-bootstrap';
 import blackAdam from '../assets/Images/Trending/black_adam.jpg'
 const Trending = () => {
     return (
         <div className='trendingContainer'>
-            <Card className="bg-dark text-white">
-            <Card.Img src={blackAdam} alt="Black Adam" className='movieImage' />
-                <Card.Title>Black Adam</Card.Title>
-                <Card.Text>
-                Seorang antihero dari Kahndaq, Black Adam, 
-                bangkit setelah ribuan tahun terkurung untuk membawa keadilan
-                 versinya sendiri, memicu bentrokan epik dengan para pahlawan 
-                 Justice Society.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card>
+            <Container>
+                <Row>
+                    <Col className='movieImage'>
+                        <Card className="bg-dark text-white text-center mb-3 movieImage">
+                        <Card.Img src={blackAdam} alt="Black Adam"  />
+                        <Card.Title className='text-center'>Black Adam</Card.Title>
+                        <Card.Text className='text-left'>
+                        Seorang antihero dari Kahndaq, Black Adam, 
+                        bangkit setelah ribuan tahun terkurung untuk membawa keadilan
+                        versinya sendiri, memicu bentrokan epik dengan para pahlawan 
+                        Justice Society.
+                        </Card.Text>
+                        <Card.Text className='text-lect'>Last updated 3 mins ago</Card.Text>
+                        </Card>  
+                        </Col>
+                </Row>
+        </Container>
+            
         </div>
     )
 }
